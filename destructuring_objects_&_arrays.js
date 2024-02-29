0cef1166aeeba28d613e58c8176d14a8e7c2ba92
Mohammed Taqi <taqealibade@gmail.com>
2024-02-29 23:38:53 +0300

    flat & flatMap

--- destructuring_objects_&_arrays.js
+++ destructuring_objects_&_arrays.js
@@ -485,3 +485,18 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(account4.movements.every(mov => mov > 0)); //* true
 
 
+//* Flat & flatMap
+// const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// console.log(arr.flat());
+
+// const arrDeep = [[[1, 2], 3, [4, [5, 6]], 7, 8]];
+// console.log(arrDeep.flat(3));
+
+const overalBalance = accounts.map(acc => acc.movements).flat().reduce((acc, mov)=> acc + mov, 0);
+console.log(overalBalance);
+
+const overalBalance2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov)=> acc + mov, 0);
+console.log(overalBalance2);
+
+
+
