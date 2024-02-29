e723419b51dd5512f3882dedcc95dd2c6b970e4d
Mohammed Taqi <taqealibade@gmail.com>
2024-03-01 00:06:52 +0300

    Implementing sorting

--- destructuring_objects_&_arrays.js
+++ destructuring_objects_&_arrays.js
@@ -137,9 +137,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (movements) => {
+const displayMovements = (movements, sort = false) => {
   containerMovements.innerHTML = '';
-  movements.forEach((mov, i) => {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -150,6 +153,8 @@ const displayMovements = (movements) => {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
+
+
 
 //Reduce Method
 const calcDisplayBalance = (acc) => {
@@ -280,6 +285,13 @@ btnClose.addEventListener('click', (e) => {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
+let sorted = false;
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+})
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -492,11 +504,46 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const arrDeep = [[[1, 2], 3, [4, [5, 6]], 7, 8]];
 // console.log(arrDeep.flat(3));
 
-const overalBalance = accounts.map(acc => acc.movements).flat().reduce((acc, mov)=> acc + mov, 0);
-console.log(overalBalance);
+// const overalBalance = accounts.map(acc => acc.movements).flat().reduce((acc, mov)=> acc + mov, 0);
+// console.log(overalBalance);
 
-const overalBalance2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov)=> acc + mov, 0);
-console.log(overalBalance2);
+// const overalBalance2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov)=> acc + mov, 0);
+// console.log(overalBalance2);
+
+
+//* Sorting with Strings
+// const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owners.sort()); //! sort mutated the array
+// console.log(owners);
+
+
+//* Sorting with Numbers
+// console.log(movements);
+
+//* return < 0 A, B (keep order)
+//* return > 0 B, A (switch order)
+
+//Ascending
+// movements.sort((a, b) => {
+//   if(a > b)
+//   return 1;
+// if(b > a)
+// return -1;
+// });
+
+// movements.sort((a, b) => a - b);
+// console.log(movements);
+
+//Descending
+// movements.sort((a, b) => {
+//   if(a > b)
+//   return -1;
+// if(b > a)
+// return 1;
+// });
+
+// movements.sort((a, b) => b - a);
+// console.log(movements);
 
 
 
