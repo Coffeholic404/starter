92c2a49fb65be2bac3173e6e078b66a32eed48ac
Mohammed Taqi <taqealibade@gmail.com>
2024-03-01 12:07:25 +0300

    exercise no.1

--- destructuring_objects_&_arrays.js
+++ destructuring_objects_&_arrays.js
@@ -547,3 +547,10 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 
 
+//* Array Methods Practice
+// 1.
+const banckDepositSum = accounts.flatMap(acc => acc.movements).filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+
+console.log(banckDepositSum);
+
+
