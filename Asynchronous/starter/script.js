3bcb9c557d9af6ee8167afda6d5849ddd92dabe7
Mohammed Taqi <taqealibade@gmail.com>
2024-03-02 22:27:27 +0300

    fix the data format so i can work with it

--- Asynchronous/starter/script.js
+++ Asynchronous/starter/script.js
@@ -2,30 +2,43 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
 ///////////////////////////////////////
 //! AJAX request the old school way (XML HTTP request)
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send(); //*fetch the data in the background
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send(); //*fetch the data in the background
 
-//* Register a callBack on the request object for the load event
-request.addEventListener('load', () => {
-  const [data] = JSON.parse(request.responseText);
-  console.log(data);
+  //* Register a callBack on the request object for the load event
+  request.addEventListener('load', () => {
+    const [data] = JSON.parse(request.responseText);
+    const {flags, name, region, languages, currencies, population} = data;
+    const currenciesArr = Object.keys(currencies).map(key => [key, currencies[key]])
+    const languagesArr = Object.keys(languages).map(key => [key, languages[key]])
+    // console.table(data);
+    console.log(data);
 
-  const html = `
+    const html = `
     <article class="country">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${languagesArr[0][1]}</p>
+      <p class="country__row"><span>💰</span>${currenciesArr[0][0]}</p>
     </div>
   </article>
     `;
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
-});
+  });
+};
+
+getCountryData('iraq')
+getCountryData('korea')
+getCountryData('japan')
+getCountryData('russia')
+getCountryData('portugal')
