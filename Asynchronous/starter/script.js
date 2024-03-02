311f01ff1e57927b126fe6b1cdc552c36b42143c
Mohammed Taqi <taqealibade@gmail.com>
2024-03-02 17:44:54 +0300

    Our First AJAX Call: XMLHttpRequest

--- Asynchronous/starter/script.js
+++ Asynchronous/starter/script.js
@@ -4,3 +4,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//! AJAX request the old school way (XML HTTP request)
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.send();//*fetch the data in the background
+
+//* Register a callBack on the request object for the load event
+request.addEventListener('load', () => console.log(request.responseText));
